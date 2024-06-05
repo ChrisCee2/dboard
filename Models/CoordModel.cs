51839a42674a6250fd0aaaa86d0c8f45c4d23b02
ChrisCee2 <chrischung411@gmail.com>
2024-06-04 22:57:47 -0400

    MA-3: Create model for coordinates

---
+++ Models/CoordModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using mystery_app.ViewModels;
+
+namespace mystery_app.Models;
+public class Coordinate
+{
+
+    public Coordinate(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    private int _x;
+    private int _y;
+
+    public int X
+    {
+        get { return _x; }
+        set { if (value > 0) _x = value; }
+    }
+    public int Y
+    {
+        get { return _y; }
+        set { if (value > 0) _y = value; }
+    }
+
+    public List<int> GetAsList()
+    {
+        return new List<int> { X, Y };
+    }
+}
