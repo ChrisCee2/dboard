dd2cf13de240a856697183e954f8ff4301fdda69
ChrisCee2 <chrischung411@gmail.com>
2024-06-04 22:58:24 -0400

    MA-3: Create view model for node

---
+++ ViewModels/NodeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using mystery_app.Models;
+
+namespace mystery_app.ViewModels;
+    
+public partial class NodeViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private NodeModel _node = new NodeModel("Chris", "He's a dude", 30, 30);
+}
