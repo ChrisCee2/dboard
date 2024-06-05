5ed65ea456805af1568abf4f564545dbcdf48d87
ChrisCee2 <chrischung411@gmail.com>
2024-06-05 01:17:27 -0400

    MA-3: Created node view

---
+++ Views/Node.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Logging;
+using Avalonia.Markup.Xaml;
+
+namespace mystery_app.Views;
+
+public partial class Node : UserControl
+{
+    public Node()
+    {
+        InitializeComponent();
+    }
+}
