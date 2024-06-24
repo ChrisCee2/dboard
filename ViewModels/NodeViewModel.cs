98d4d472abcb4d771463aa2a021808ad96c45ad9
ChrisCee2 <chrischung411@gmail.com>
2024-06-24 00:19:57 -0400

    MA-12: Add constructors to node view model

--- ViewModels/NodeViewModel.cs
+++ ViewModels/NodeViewModel.cs
@@ -6,6 +6,27 @@ namespace mystery_app.ViewModels;
 public partial class NodeViewModel : ObservableObject
 {
 
+    public NodeViewModel()
+    {
+        _name = "";
+        _desc = "";
+        _edges = new Dictionary<NodeViewModel, string>();
+    }
+
+    public NodeViewModel(string name="", string desc="", Dictionary<NodeViewModel, string> edges=null)
+    {
+        _name = name;
+        _desc = desc;
+        if (edges != null) 
+        {
+            _edges = edges;
+        }
+        else
+        {
+            _edges = new Dictionary<NodeViewModel, string>();
+        }
+    }
+
     [ObservableProperty]
     private string _name;
 
