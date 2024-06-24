8973523b898dcc52b88a1ac381c45e8916f477f3
ChrisCee2 <chrischung411@gmail.com>
2024-06-24 00:20:46 -0400

    MA-12: Remove press interaction from workspace

--- Views/Workspace.axaml.cs
+++ Views/Workspace.axaml.cs
@@ -1,8 +1,4 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Input;
-using CommunityToolkit.Mvvm.ComponentModel;
-using mystery_app.ViewModels;
 
 namespace mystery_app.Views;
 
@@ -12,41 +8,4 @@ public partial class Workspace : UserControl
     {
         InitializeComponent();
     }
-
-    private NodeViewModel _selectedNode;
-
-    public void helloworld() { return; }
-
-    private void PointerPressedHandler(object sender, PointerPressedEventArgs args)
-    {
-        //// If not left click, return
-        //if (!e.GetCurrentPoint(Parent as Avalonia.Visual).Properties.IsLeftButtonPressed) { return; }
-        //_isPressed = true;
-        //base.OnPointerPressed(e);
-
-        var point = args.GetCurrentPoint(sender as Control);
-        var x = point.Position.X;
-        var y = point.Position.Y;
-        var msg = $"Pointer press at {x}, {y} relative to sender.";
-        if (point.Properties.IsLeftButtonPressed)
-        {
-            msg += " Left button pressed.";
-        }
-        if (point.Properties.IsRightButtonPressed)
-        {
-            msg += " Right button pressed.";
-        }
-
-        if (sender is Node node)
-        {
-            results.Text = "YO";
-            return;
-        }
-        if (args == null) { return; }
-        results.Text = args.Source.ToString();
-        //var a = sender;
-        //object data = a.DataContext;
-        //results.Text = data.ToString();
-    }
-
 }
