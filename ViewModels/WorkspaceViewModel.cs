4d7aadb8da41cd13a9fc51c4a26aeb4caa8cdd19
ChrisCee2 <chrischung411@gmail.com>
2024-06-24 00:21:47 -0400

    MA-12: Add functionality to create edges between nodes

--- ViewModels/WorkspaceViewModel.cs
+++ ViewModels/WorkspaceViewModel.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using mystery_app.Messages;
 
@@ -10,6 +9,9 @@ namespace mystery_app.ViewModels;
 public partial class WorkspaceViewModel : ViewModelBase
 {
 
+    private NodeViewModel? _selectedNode;
+    private NodeViewModel? _enteredNode;
+
     public WorkspaceViewModel()
     {
         Nodes = new ObservableCollection<NodeViewModel>(new List<NodeViewModel>());
@@ -17,6 +19,35 @@ public partial class WorkspaceViewModel : ViewModelBase
         WeakReferenceMessenger.Default.Register<CreateNodeMessage>(this, (sender, message) =>
         {
             CreateNode();
+        });
+        
+        WeakReferenceMessenger.Default.Register<SelectNodeEdgeMessage>(this, (sender, message) =>
+        {
+            if (Nodes.Contains(message.Value))
+            {
+                _selectedNode = message.Value;
+            }
+        });
+        
+        WeakReferenceMessenger.Default.Register<ReleaseNodeEdgeMessage>(this, (sender, message) =>
+        {
+            _enteredNode = message.Value;
+
+            if (Nodes.Contains(_enteredNode) && Nodes.Contains(_selectedNode) && _selectedNode != null && _enteredNode != null && !Object.Equals(_enteredNode, _selectedNode))
+            {
+                if (_selectedNode.Edges.ContainsKey(_enteredNode))
+                {
+                    _selectedNode.Edges.Remove(_enteredNode);
+                }
+                if (_enteredNode.Edges.ContainsKey(_selectedNode))
+                {
+                    _enteredNode.Edges.Remove(_selectedNode);
+                }
+                _selectedNode.Edges.Add(_enteredNode, "reason");
+                _enteredNode.Edges.Add(_selectedNode, "reason");
+            }
+            _selectedNode = null;
+            _enteredNode = null;
         });
     }
 
--- Views/Node.axaml.cs
+++ Views/Node.axaml.cs
@@ -1,4 +1,11 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.VisualTree;
+using CommunityToolkit.Mvvm.Messaging;
 using mystery_app.Controls;
+using mystery_app.Messages;
+using mystery_app.ViewModels;
 
 namespace mystery_app.Views;
 
@@ -7,5 +14,25 @@ public partial class Node : MovableUserControl
     public Node()
     {
         InitializeComponent();
+    }
+
+    private void SelectNodeEdge(object sender, PointerPressedEventArgs args)
+    {
+        WeakReferenceMessenger.Default.Send(new SelectNodeEdgeMessage((NodeViewModel)DataContext));
+    }
+
+    private void ReleaseNodeEdge(object sender, PointerReleasedEventArgs args)
+    {
+        var root = (TopLevel)((Visual)args.Source).GetVisualRoot();
+        var rootCoordinates = args.GetPosition(root);
+        var hitElement = root.InputHitTest(rootCoordinates);
+
+        if (hitElement is Control control)
+        {
+            // TODO Find node, should improve this later
+            var node = control.Parent.Parent.Parent;
+            if (node == null || node.DataContext == null || node.DataContext is not NodeViewModel) { return; }
+            WeakReferenceMessenger.Default.Send(new ReleaseNodeEdgeMessage((NodeViewModel)node.DataContext));
+        }
     }
 }
