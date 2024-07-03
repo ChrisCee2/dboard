61fb1db3a83075d201eeb25aeb2e97f9003921cc
ChrisCee2 <chrischung411@gmail.com>
2024-07-02 23:31:41 -0400

    MA-35: Added resize button to nodes

--- Views/Node.axaml.cs
+++ Views/Node.axaml.cs
@@ -12,16 +12,32 @@ namespace mystery_app.Views;
 
 public partial class Node : MovableUserControl
 {
+
+    private bool _resizing;
+    private Point _lastPressedPoint;
+
     public Node()
     {
         InitializeComponent();
     }
 
+    // Updates position of the node to the node viewmodel
+    private void MoveNodeHandler(object sender, PointerEventArgs args)
+    {
+        var workspaceRoot = ((Control)((Control)sender).Parent).PointToScreen(new Point(0, 0));
+        var senderRoot = ((Control)sender).PointToScreen(new Point(0, 0));
+        var pos = new Point(senderRoot.X - workspaceRoot.X, senderRoot.Y - workspaceRoot.Y);
+        var message = new MoveNodeHelper((NodeViewModel)DataContext, pos);
+        WeakReferenceMessenger.Default.Send(new MoveNodeMessage(message));
+    }
+
+    // On selecting node edge creation, tell workspace this node has been selected
     private void SelectNodeEdge(object sender, PointerPressedEventArgs args)
     {
         WeakReferenceMessenger.Default.Send(new SelectNodeEdgeMessage((NodeViewModel)DataContext));
     }
 
+    // On releasing node edge creation, find node released on and send to workspace
     private void ReleaseNodeEdge(object sender, PointerReleasedEventArgs args)
     {
         var root = (TopLevel)((Visual)args.Source).GetVisualRoot();
@@ -37,12 +53,24 @@ public partial class Node : MovableUserControl
         }
     }
 
-    private void MoveNodeHandler(object sender, PointerEventArgs args)
+    // On selecting resize button, get current cursor position
+    private void SelectResize(object sender, PointerPressedEventArgs args)
     {
-        var workspaceRoot = ((Control)((Control)sender).Parent).PointToScreen(new Point(0, 0));
-        var senderRoot = ((Control)sender).PointToScreen(new Point(0, 0));
-        var pos = new Point(senderRoot.X - workspaceRoot.X, senderRoot.Y - workspaceRoot.Y);
-        var message = new MoveNodeHelper((NodeViewModel)DataContext, pos);
-        WeakReferenceMessenger.Default.Send(new MoveNodeMessage(message));
+        var root = (TopLevel)((Visual)args.Source).GetVisualRoot();
+        _resizing = true;
+        _lastPressedPoint = args.GetPosition(root);
+    }
+
+    // On releasing resize button, get new size from difference between initial and current cursor position
+    private void ReleaseResize(object sender, PointerReleasedEventArgs args)
+    {
+        if (_resizing)
+        {
+            _resizing = false;
+            var root = (TopLevel)((Visual)args.Source).GetVisualRoot();
+            var resizeSize = args.GetPosition(root) - _lastPressedPoint;
+            Width = (Width + resizeSize.X < this.MinWidth) ? this.MinWidth : Width + resizeSize.X;
+            Height = (Height + resizeSize.Y < this.MinHeight) ? this.MinHeight : Height + resizeSize.Y;
+        }
     }
 }
