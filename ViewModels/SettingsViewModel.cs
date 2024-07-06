b679f0f10e8417ff58784ca4a4035e769aa70f48
ChrisCee2 <chrischung411@gmail.com>
2024-07-05 23:00:30 -0400

    MA-30: Can switch between settings and main content pages

---
+++ ViewModels/SettingsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using mystery_app.Messages;
+
+namespace mystery_app.ViewModels;
+    
+public partial class SettingsViewModel : ObservableObject
+{
+
+    public SettingsViewModel()
+    {
+    }
+
+    [RelayCommand]
+    private void GoToMainContent()
+    {
+        WeakReferenceMessenger.Default.Send(new ChangePageMessage("MainContent"));
+    }
+
+}
