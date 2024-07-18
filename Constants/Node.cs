23ff00cf3ce758af2eadc742663d30a172fe5f36
ChrisCee2 <chrischung411@gmail.com>
2024-07-17 23:24:25 -0400

    MA-46: Refactor NodeUserControl

--- Constants/Node.cs
+++ Constants/Node.cs
@@ -1,6 +1,11 @@
-﻿namespace mystery_app.Constants;
+﻿using System.Collections.ObjectModel;
+
+namespace mystery_app.Constants;
 
 public static class Node
 {
     public static int EDGE_THICKNESS => 2;
+    public static string EDGE_BUTTON_TAG => "EdgeButton";
+    public static string NONMOVABLE_TAG => "Nonmovable";
+    public static Collection<string> NONMOVABLE_TAGS => new Collection<string>() { EDGE_BUTTON_TAG, NONMOVABLE_TAG };
 }
