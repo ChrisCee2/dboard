003c78710bb96b70af8fe63fc4ea60725b95ad62
ChrisCee2 <chrischung411@gmail.com>
2024-07-24 20:01:58 -0400

    MA-51: Created view and viewmodel for edge

---
+++ ViewModels/EdgeViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
+using mystery_app.Constants;
+using mystery_app.Models;
+
+namespace mystery_app.ViewModels;
+
+public partial class EdgeViewModel : ObservableObject
+{
+
+    public EdgeViewModel(
+        NodeViewModelBase fromNode,
+        NodeViewModelBase toNode,
+        string description = "",
+        double thickness = EdgeConstants.THICKNESS)
+    {
+        _edge = new EdgeModel(fromNode, toNode, description, thickness, EdgeConstants.COLOR);
+    }
+
+    public EdgeViewModel(
+        NodeViewModelBase fromNode,
+        NodeViewModelBase toNode,
+        Color color,
+        string description = "",
+        double thickness = EdgeConstants.THICKNESS)
+    {
+        _edge = new EdgeModel(fromNode, toNode, description, thickness, color);
+    }
+
+    [ObservableProperty]
+    private EdgeModel _edge;
+
+}
