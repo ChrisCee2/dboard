1ff5c031d58924919f06441eeba3c5512ac4106a
ChrisCee2 <chrischung411@gmail.com>
2024-08-04 15:14:07 -0400

    MA-11: Add reference handling for saving

---
+++ Tools/RefHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace mystery_app.Tools;
+
+class RefHandler : ReferenceHandler
+{
+    public RefHandler() => Reset();
+    private ReferenceResolver? _rootedResolver;
+    public override ReferenceResolver CreateResolver() => _rootedResolver!;
+    public void Reset() => _rootedResolver = new RefResolver();
+}
--- Views/MainContentView.axaml.cs
+++ Views/MainContentView.axaml.cs
@@ -1,16 +1,26 @@
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Logging;
 using Avalonia.Platform.Storage;
 using mystery_app.Models;
+using mystery_app.Tools;
 using mystery_app.ViewModels;
 
 namespace mystery_app.Views;
 
 public partial class MainContentView : UserControl
 {
+    JsonSerializerOptions options = new()
+    {
+        WriteIndented = true
+    };
+
     public MainContentView()
     {
         InitializeComponent();
@@ -37,12 +47,18 @@ public partial class MainContentView : UserControl
         {
             // Open writing stream from the file.
             await using var stream = await file.OpenWriteAsync();
-            //using var streamWriter = new StreamWriter(stream);
+            options.ReferenceHandler = new RefHandler();
 
-            ObservableCollection<NodeViewModelBase> nodes = ((MainContentViewModel)DataContext).Workspace.Nodes;
-            EdgeCollectionModel edges = ((MainContentViewModel)DataContext).Workspace.Edges;
-            JsonSerializer.SerializeAsync(stream, nodes);
-            JsonSerializer.SerializeAsync(stream, edges);
+            List<NodeModelBase> nodes = ((MainContentViewModel)DataContext).Workspace.Nodes.Select(x => x.NodeBase).ToList();
+            List<EdgeModel> edges = ((MainContentViewModel)DataContext).Workspace.Edges.Select(x => x.Edge).ToList();
+            foreach (var node in nodes)
+            {
+                JsonSerializer.SerializeAsync(stream, node, options);
+            }
+            foreach (var edge in edges)
+            {
+                JsonSerializer.SerializeAsync(stream, edge, options);
+            }
         }
     }
 }
