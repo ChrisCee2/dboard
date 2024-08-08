d35b305590592a5007ac2014bb6cb614b61eb47b
ChrisCee2 <chrischung411@gmail.com>
2024-08-08 01:15:19 -0400

    MA-24: Highlight node on select

---
+++ Converters/BoolToDoubleConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+
+namespace mystery_app.Converters;
+public class BoolToDoubleConverter : IValueConverter
+{
+    public static readonly BoolToDoubleConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool val)
+        {
+            return (bool)value ? double.Parse((string)parameter) : 0;
+        }
+        return null;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return null;
+    }
+
+}
