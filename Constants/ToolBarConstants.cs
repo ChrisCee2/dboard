8fc090ee287edd0fdffa5d991a86248a860ddfb0
ChrisCee2 <chrischung411@gmail.com>
2024-08-10 18:54:39 -0400

    MA-27: Made toolbar look nicer

--- Constants/ToolBarConstants.cs
+++ Constants/ToolBarConstants.cs
@@ -6,10 +6,11 @@ namespace mystery_app.Constants;
 
 public static class ToolBarConstants
 {
+    public const double FONT_SIZE = 12;
     public static readonly ImmutableSolidColorBrush COLOR = new ImmutableSolidColorBrush(new Color(170, 150, 150, 150));
     public static readonly ImmutableSolidColorBrush BUTTON_BACKGROUND = new ImmutableSolidColorBrush(new Color(0, 0, 0, 0));
-    public static readonly Thickness WORKSPACE_NORM_PAD = new Thickness(0, 0, 0, 0);
-    public static readonly Thickness WORKSPACE_MAX_PAD = new Thickness(8, 0, 8, 0);
+    public const double MAX_BORDER_WIDTH = 18;
+    public const double NORM_BORDER_WIDTH = 10;
     public static readonly Thickness MAX_MBTN_PAD = new Thickness(12, 16, 12, 8);
     public static readonly Thickness NORM_MBTN_PAD = new Thickness(12, 8, 12, 8);
     public static readonly Thickness MAX_LPAD = new Thickness(20, 16, 12, 8);
