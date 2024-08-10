e085da2b65d296f3b1bd3bcaae8ba05f66d8f638
ChrisCee2 <chrischung411@gmail.com>
2024-08-09 21:14:59 -0400

    MA-27: Toolbar slightly opaque

---
+++ Constants/ToolBarConstants.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia.Media;
+using Avalonia.Media.Immutable;
+
+namespace mystery_app.Constants;
+
+public static class ToolBarConstants
+{
+    public static readonly ImmutableSolidColorBrush COLOR = new ImmutableSolidColorBrush(new Color(170, 150, 150, 150));
+}
