f2c7b609b77775a5749e2bc1879df105d75b0184
ChrisCee2 <chrischung411@gmail.com>
2024-08-19 22:14:58 -0400

    MA-77: Button to add edge description

---
+++ Converters/ShowEdgeAddDescConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace mystery_app.Converters;
+public class ShowEdgeAddDescConverter : IMultiValueConverter
+{
+    public static readonly ShowEdgeAddDescConverter Instance = new();
+
+    public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values.Count == 2 && values[0] is null && values[1] is bool isSelected && isSelected)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return null;
+    }
+
+}
