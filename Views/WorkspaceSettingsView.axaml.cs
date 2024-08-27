224a759d60772e76964484c213d94c4c203a13ec
ChrisCee2 <chrischung411@gmail.com>
2024-08-27 00:10:00 -0400

    MA-97: Set workspace image

--- Views/WorkspaceSettingsView.axaml.cs
+++ Views/WorkspaceSettingsView.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Platform.Storage;
+using Avalonia.VisualTree;
 using mystery_app.ViewModels;
 
 namespace mystery_app.Views;
@@ -16,18 +17,32 @@ public partial class WorkspaceSettingsView : UserControl
     {
         var files = await TopLevel.GetTopLevel(this).StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
         {
-            Title = "Insert Image",
+            Title = "Select Image",
             AllowMultiple = false
         });
 
         if (files.Count == 1)
         {
-            ((WorkspaceViewModel)DataContext).CanvasImagePath = files[0].Path.LocalPath;
+            if (((Control)sender).FindAncestorOfType<StackPanel>().Name == "CanvasImage")
+            {
+                ((WorkspaceViewModel)DataContext).CanvasImagePath = files[0].Path.LocalPath;
+            }
+            else if (((Control)sender).FindAncestorOfType<StackPanel>().Name == "WorkspaceImage")
+            {
+                ((WorkspaceViewModel)DataContext).WorkspaceImagePath = files[0].Path.LocalPath;
+            }
         }
     }
 
     public void RemoveImage(object sender, RoutedEventArgs args)
     {
-        ((WorkspaceViewModel)DataContext).CanvasImagePath = null;
+        if (((Control)sender).FindAncestorOfType<StackPanel>().Name == "CanvasImage")
+        {
+            ((WorkspaceViewModel)DataContext).CanvasImagePath = null;
+        }
+        else if (((Control)sender).FindAncestorOfType<StackPanel>().Name == "WorkspaceImage")
+        {
+            ((WorkspaceViewModel)DataContext).WorkspaceImagePath = null;
+        }
     }
 }
