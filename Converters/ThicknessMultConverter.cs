b4a4d4324ea1a672bcc86be5d4d8c043081f79bf
ChrisCee2 <chrischung411@gmail.com>
2024-08-30 11:49:27 -0400

    MA-112: Added default icon and edit button visual

---
+++ Converters/ThicknessMultConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace mystery_app.Converters;
+public class ThicknessMultConverter : IMultiValueConverter
+{
+    public static readonly ThicknessMultConverter Instance = new();
+
+    public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values[0] is double padding && values[1] is double factor)
+        {
+            return new Avalonia.Thickness(padding * factor);
+        }
+        return new Avalonia.Thickness(0);
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return null;
+    }
+
+}
