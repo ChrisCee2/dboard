85b15b9ed95cdd519a9c2261e70ca8989987d223
ChrisCee2 <chrischung411@gmail.com>
2025-06-23 21:44:36 -0400

    MA-129: Fixed scaling of color changer, will put translation issue in another ticket

---
+++ Converters/ReverseScaleConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data.Converters;
+using Avalonia.Logging;
+
+namespace dboard.Converters;
+public class ReverseScaleConverter : IValueConverter
+{
+    public static readonly ReverseScaleConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is double scale)
+        {
+            Logger.TryGet(LogEventLevel.Fatal, LogArea.Control)?.Log(this, (Math.Round(1.0 / scale, 3)).ToString());
+            return Math.Round(1.0 / scale, 3);
+        }
+
+        return 1.0;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return 1.0;
+    }
+
+}
