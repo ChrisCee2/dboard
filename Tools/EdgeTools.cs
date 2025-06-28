0cd906de641f0956a665c60f4fe904da554ab2f3
ChrisCee2 <chrischung411@gmail.com>
2025-06-27 21:12:57 -0400

    MA-167: Shortened edge length to make clicking pin easier

--- Tools/EdgeTools.cs
+++ Tools/EdgeTools.cs
@@ -7,8 +7,13 @@ class EdgeTools
 {
 
     // Get point where edge should appear given a node's coordinates and width
-    public static Point EdgePosFromNode(double x, double y, double width)
+    public static Point EdgePosFromNode(double x, double y, double width, double endX = 0, double endY = 0)
     {
-        return new Point(x + (width / 2), y + EdgeConstants.distFromEdgeToNodePos);
+        Vector offset = new Vector(0, 0);
+        if (endX != 0 && endY != 0)
+        {
+            offset = new Vector(endX - x, endY - y).Normalize();
+        }
+        return new Point(x + (width / 2), y + EdgeConstants.distFromEdgeToNodePos) + new Point(offset.X, offset.Y) * 5;
     }
 }
